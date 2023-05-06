46b4240a5c48dd1d2f3d85b48a6e8b9bef51e97c
Simon Ågren <simon.agren@gmail.com>
2023-05-06 12:17:58 +0200

    feat(creational pattern): builder

--- Start/Creational/Builder/builder.go
+++ Start/Creational/Builder/builder.go
@@ -1,14 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 // The NotificationBuilder has fields exported
 type NotificationBuilder struct {
-	Title    string
-	SubTitle string
-	Message  string
-	Image    string
-	Icon     string
-	Priority int
-	NotType  string
+	Title     string
+	SubTitle  string
+	Message   string
+	Image     string
+	Icon      string
+	Priority  int
+	NotifType string
 }
 
 func newNotificationBuilder() *NotificationBuilder {
@@ -39,15 +43,24 @@ func (nb *NotificationBuilder) SetPriority(pri int) {
 	nb.Priority = pri
 }
 
-func (nb *NotificationBuilder) SetType(notType string) {
-	nb.NotType = notType
+func (nb *NotificationBuilder) SetType(notifType string) {
+	nb.NotifType = notifType
 }
 
 // The Build method returns a fully finished Notification object
 func (nb *NotificationBuilder) Build() (*Notification, error) {
 	// TODO: Error checking can be done at the Build stage
-
+	if nb.Title == "" {
+		return nil, fmt.Errorf("no title")
+	}
 	// TODO: Return a newly created Notification object using the current settings
-
-	return nil, nil
+	return &Notification{
+		title:    nb.Title,
+		subtitle: nb.SubTitle,
+		message:  nb.Message,
+		image:    nb.Image,
+		icon:     nb.Icon,
+		priority: nb.Priority,
+		notType:  nb.NotifType,
+	}, nil
 }
--- Start/Creational/Builder/example.go
+++ Start/Creational/Builder/example.go
@@ -1,10 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	// TODO: Create a NotificationBuilder and use it to set properties
+	nb := newNotificationBuilder()
 
 	// TODO: Use the builder to set some properties
+	// Since the fields of NotificationBuilder are exported,
+	// it is not necessary to use Setters.
+	nb.SetTitle("Notif Title")
+	nb.SetPriority(1)
 
 	// TODO: Use the Build function to create a finished object
-
+	n, err := nb.Build()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", n)
 }
